55eb316ad93e698bb6910cd632cd8cfa05a50b9c
GeovanaQ7 <geovanaq60@gmail.com>
2024-04-17 23:57:27 -0300

    atualização

--- Banco-24horas.py
+++ Banco-24horas.py
@@ -1,7 +1,3 @@
-print("hello word")
-print("qualquer outra coisa")
-
-
 menu='''
      
       [d]-Depositar
@@ -22,12 +18,42 @@ while True:
 
     opcao= input(menu)
     if opcao == "d":
-        print("Deposito")   
+        valor= float(input("Informe o valor do deposito:"))
+
+        if valor >0:
+            saldo<= valor
+            extrato = f"Deposito: R$(valor:.2f)\n"
+        else:
+            print ("Operação inválida, por favor selecione a operação desejada.")    
+
+    
+    
     elif opcao == "s":
-        print("Sacar")
-    elif opcao == "e":
-        print("Extrato")  
-    elif opcao == "q":
-       break
+        valor= float(input("Informe o valor do saque:"))
+        excedeu_saldo = valor > saldo
+        excedeu_limite = valor > limite
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+        if excedeu_saldo:
+            print ("Operação inválida, por favor selecione a operação desejada.")    
+        if excedeu_limite:
+            print ("Operação inválida, por favor selecione a operação desejada.")    
+        if excedeu_saques:
+            print ("Operação inválida, por favor selecione a operação desejada.")    
+
+        elif valor > 0:
+             saldo -= valor
+             extrato >= f"Saque: R$(valor:.2f)\n"
+        else:
+            print("Operação falhou,pois o valor informado é invalido.")
+
+    elif opcao == "e":  
+         print("\n================ EXTRATO ================")
+         print("Não foram realizadas movimentações." if not extrato else extrato)
+         print(f"\nSaldo:\t\tR$ {saldo:.2f}")
+         print("==========================================")
+         
+    elif opcao== "q":
+        break
+        
     else:
-        print("Operação inválida, por favor selecione a operação desejada.")
+        print("Operação inválida, por favor selecione a operação desejada.")      
